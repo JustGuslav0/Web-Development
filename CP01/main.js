f4210269f8af3ee0b399f678fcb1dae6994414ea
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 22:38:04 -0300

    erro

--- CP01/main.js
+++ CP01/main.js
@@ -9,12 +9,32 @@ function calcular(){
 
     let min1 = Math.floor(minIni.value)
     let hora1 = Math.floor(horaIni.value)*60
-
+    
     let min2 = Math.floor(minFim.value)
     let hora2 =  Math.floor(horaFim.value)*60
 
-    let horario1 = Math.floor(hora1+min1)
-    let horario2 = Math.floor(hora2+min2)
+    let horario1
+    let horario2
+
+    if(hora1<=-1||hora1>=24||min1<=-1 || min1>=60){
+        alert('Horário de início inválido, favor insira novamente')
+    }
+    else{
+        if(hora2<=-1||hora2>=24||min2<=-1 || min2>=60){
+            alert('Horário de término inválido, favor insira novamente')
+        }
+    else{
+        horario1 = Math.floor(hora1+min1)
+        horario2 = Math.floor(hora2+min2)
+    }
+    }
+
+    
+
+
+
+
+
 
     let matHr
     let matMin
@@ -22,10 +42,10 @@ function calcular(){
 
     // horario1 = 20
     // horario2 = 00
-    // 24 - 
+    // 24 - 20
 
     if(horario2<horario1){
-        // diferença entre o horario do começo e do final da reunião, quando o primeiro e maior que o segundo
+        // Diferença entre o horario do começo e do final da reunião, quando o primeiro e maior que o segundo
         horasDiferenca = horario1-horario2
         matHr = Math.floor((24*60-horasDiferenca)/60)
         matMin = Math.floor((24*60-horasDiferenca)%60)
@@ -34,8 +54,6 @@ function calcular(){
         matHr = Math.floor((horario2-horario1)/60)
         matMin = Math.floor((horario2-horario1)%60)
     }
-
-    console.log(matHr)
 
     if(matHr<10){
     atrHr.innerText = "0" + matHr
@@ -50,7 +68,6 @@ function calcular(){
     else{
     atrMin.innerText = matMin
     }
-
 
 
 }
