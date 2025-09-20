53587747df0ef8b686114694d7c1a19d6966af4f
JustGuslav0 <galmeida.senai11@gmail.com>
2025-09-20 16:43:45 -0300

    CP01 - Último commit

--- CP01/main.js
+++ CP01/main.js
@@ -1,68 +1,69 @@
 function calcular(){
-    let horaIni = document.getElementById('horaIni')
-    let minIni = document.getElementById('minIni')
-    let horaFim = document.getElementById('horaFim')
-    let minFim = document.getElementById('minFim')
+    // Valor das divs do HTML em variáveis aqui no JS
+        // O número colocado no input das horas é transformado em minutos
+    let horaIni = ((document.getElementById('horaIni')).value)*60
+    let minIni = ((document.getElementById('minIni')).value)
+    let horaFim = ((document.getElementById('horaFim')).value)*60
+    let minFim = ((document.getElementById('minFim')).value)
     let atrHr = document.getElementById('horas')
-    let atrMin = document.getElementById('minutos')
-
-    let min1 = Math.floor(minIni.value)
-    let hora1 = Math.floor(horaIni.value)*60
-    
-    let min2 = Math.floor(minFim.value)
-    let hora2 =  Math.floor(horaFim.value)*60
-
+    let atrMin = document.getElementById('minutos')  
+o
+    // Variáveis criadas fora do if para caso precise usar elas novamente
     let horario1
     let horario2
 
-    if(hora1<=-1||hora1>=24*60||min1<=-1 || min1>=60 && hora2<=-1||hora2>=24*60||min2<=-1 || min2>=60){
+    // Caso o número digitado seja menor (ou maior) que as horas e minutos possíveis, alerta o user para que digite novamente
+        // Neste caso, se o horário de início e término estiver errado, ele avisa que ambos estão
+    if((horaIni<=-1||horaIni>=24*60||minIni<=-1 || minIni>=60) && (horaFim<=-1||horaFim>=24*60||minFim<=-1 || minFim>=60)){
         alert('Horário de início e de término inválido, favor insira novamente')
+        return
     }
+    // Caso não seja os 2, irá testar no horário de início e término, apontando em qual está o erro
     else{
-        if(hora1<=-1||hora1>=24*60||min1<=-1 || min1>=60){
+        if(horaIni<=-1||horaIni>=24*60||minIni<=-1 || minIni>=60){
             alert('Horário de início inválido, favor insira novamente')
+            return
         }
         else{
-            if(hora2<=-1||hora2>=24*60||min2<=-1 || min2>=60){
+            if(horaFim<=-1||horaFim>=24*60||minFim<=-1 || minFim>=60){
                 alert('Horário de término inválido, favor insira novamente')
+                return
             }
+            // Caso não tenha nenhum erro na digitação, prosseguimos com o cálculo
             else{
-                horario1 = Math.floor(hora1+min1)
-                horario2 = Math.floor(hora2+min2)
+                horario1 = Math.floor(horaIni+minIni)
+                horario2 = Math.floor(horaFim+minFim)
             }
 
         }
     }
-
-    
-
-
-
-
-
 
     let matHr
     let matMin
     let horasDiferenca
 
-    // horario1 = 20
-    // horario2 = 00
-    // 24 - 20
-
+    // Caso o user coloque o horário inicial maior que o horário de término
     if(horario2<horario1){
         // Diferença entre o horario do começo e do final da reunião, quando o primeiro e maior que o segundo
         horasDiferenca = horario1-horario2
+        // Adiciono um dia para o cálculo, pois se uma reunião começou as 3 e terminou as 2, acabou no outro dia
+            // Retorno o número inteiro, deixando uma sobra para serem calculado os minutos
         matHr = Math.floor((24*60-horasDiferenca)/60)
+        // %60 faz com que os minutos que passem de uma 60, sejam armazenados separados para serem adicionados no 'minutos' do html
         matMin = Math.floor((24*60-horasDiferenca)%60)
     }
+    // Se não, vai calcular normal
     else{
         matHr = Math.floor((horario2-horario1)/60)
         matMin = Math.floor((horario2-horario1)%60)
     }
 
+    // Como o JS irá passar um valor sem o 0 à esquerda(9 ao invés de 09), faço com que se caso seja < 10, adicionar um 0, se tornando idêntico a um horário
+
     if(matHr<10){
     atrHr.innerText = "0" + matHr
     }
+    // Caso não seja, apenas vai escrever o número normal
     else{
     atrHr.innerText = matHr
     }
